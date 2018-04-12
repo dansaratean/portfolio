edb378baf70273bc289f6ffaf837c14dc66ab08d
dansaratean <dansaratean10@gmail.com>
2018-04-12 17:10:42 +0200

    contact page almost finished

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material.module';
 import { routing } from './app.routing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 export const imports = [
   BrowserModule, BrowserAnimationsModule, AppMaterialModule,
-  routing,
+  routing, ReactiveFormsModule, FormsModule
 ];
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,24 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { FormControl, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContactComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  contactForm = new FormGroup({
+    name: new FormControl(),
+    message: new FormControl(),
+    checkbox: new FormControl(),
+    select: new FormControl()
+  });
+
+  isChecked: boolean;
+
+  contact = {
+    name: 'dan saratean',
+    email: 'dansaratean10@gmail.com',
+    address: 'Somewhere in Floresti',
+    phone: 'xxxxxxxxx',
+  };
 
   constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
-  contact = {
-    name: 'Dan',
-    surname: 'Saratean',
-    email: 'dansaratean10@gmail.com',
-    address: 'Somewhere in Floresti',
-    phone: 'xxxxxxxxx',
-  };
+  // getErrorMessage() {
+  //   return this.contactForm['email'].hasError('required') ? 'You must enter a value' :
+  //       this.contactForm['email'].hasError('email') ? 'Not a valid email' : '';
+  // }
 
 }
